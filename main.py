a2084779c5588c348c574586984c732ccfc0570e
aont <aont50@gmail.com>
2011-08-01 23:02:27 +0900

    small fixes

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-#    Copyright (C) 2006  Andrew Straw  <strawman@astraw.com>
-#
-#    This program can be distributed under the terms of the GNU LGPL.
-#    See the file COPYING.
-#
-
 import os, stat, errno
-# pull in some spaghetti to make this stuff work without fuse-py being installed
+
 try:
     import _find_fuse_parts
 except ImportError:
@@ -23,13 +17,35 @@ if not hasattr(fuse, '__version__'):
 
 fuse.fuse_python_api = (0, 2)
 
-def get_image(num):
-    gp = subprocess.Popen("/opt/local/bin/gnuplot", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    gp.stdin.write("set terminal png\n")
-    gp.stdin.write("plot sin(x+%s)\n" % (num*0.1))
-    gp.stdin.write("exit\n")
-    return gp.stdout.read()
+class ImageList:
+    def __init__(self):
+        self.image_list = []
 
+    def get_image(self, num):
+        for gpimage in self.image_list:
+            if gpimage.num == num:
+                return gpimage
+        gpimage = GPImage(num)
+        self.image_list.append(gpimage)
+        return gpimage
+
+    def del_image(self, num):
+        for gpimage in self.image_list:
+            if gpimage.num == num:
+                self.image_list.remove(gpimage)
+                return
+
+image_list = ImageList()
+
+class GPImage:
+    def __init__(self, num):
+        self.num = num
+        gp = subprocess.Popen("/opt/local/bin/gnuplot", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+        gp.stdin.write("set terminal png\n")
+        gp.stdin.write("plot sin(x+%s)\n" % (num*0.1))
+        gp.stdin.write("exit\n")
+        self.data = gp.stdout.read()
+    
 class MyStat(fuse.Stat):
     def __init__(self):
         self.st_mode = 0
@@ -44,36 +60,48 @@ class MyStat(fuse.Stat):
         self.st_ctime = 0
 
 
-import re
-fn_re = re.compile("/image_(\d+)\.png")
 
 
-class HelloFS(Fuse):
+
+class ExeFS(Fuse):
+    import re
+    fn_re = re.compile("/image_(\d+)\.png")
+
+    def __init__(self, *args, **kw):
+        Fuse.__init__(self, *args, **kw)
+        
+        
     def getattr(self, path):
-        fn_match = fn_re.match(path)
+        print "getattr:\t%s" % path        
+        #fn_match = ExeFS.fn_re.match(path)
+        fn_match = self.__class__.fn_re.match(path)
         st = MyStat()
         if path == '/':
             st.st_mode = stat.S_IFDIR | 0755
             st.st_nlink = 2
         elif not fn_match == None:
-            st.st_mode = stat.S_IFREG | 0444
-            st.st_nlink = 1
             image_num = int(fn_match.group(1))
             if image_num > 314:
                 return -errno.ENOENT
-            image = get_image(image_num)
-            st.st_size = len(image)
+
+            st.st_mode = stat.S_IFREG | 0444
+            st.st_nlink = 1
+
+            gpimage = image_list.get_image(image_num)
+            st.st_size = len(gpimage.data)
+            
         else:
             return -errno.ENOENT
         return st
 
     def readdir(self, path, offset):
+        print "readdir:\t%s" % path
         for r in  '.', '..', "image_0.png", "image_1.png", "image_2.png":
             yield fuse.Direntry(r)
 
     def open(self, path, flags):
         print "open:\t%s" % path
-        fn_match = fn_re.match(path)
+        fn_match = self.__class__.fn_re.match(path)
         if fn_match == None:
             return -errno.ENOENT
         accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
@@ -81,30 +109,40 @@ class HelloFS(Fuse):
             return -errno.EACCES
 
     def read(self, path, size, offset):
-        fn_match = fn_re.match(path)
+        print "read:\t%s" % path
+        fn_match = self.__class__.fn_re.match(path)
         if fn_match == None:
             return -errno.ENOENT
 
         image_num = int(fn_match.group(1))
-        image = get_image(image_num)        
-        slen = len(image)
+        gpimage = image_list.get_image(image_num)        
+        slen = len(gpimage.data)
         if offset < slen:
             if offset + size > slen:
                 size = slen - offset
-            buf = image[offset:offset+size]
+            buf = gpimage.data[offset:offset+size]
         else:
             buf = ''
         return buf
+    
+    def release(self, path, flags):
+        print "release:\t%s" % path
+        fn_match = self.__class__.fn_re.match(path)
+        if fn_match == None:
+            return -errno.ENOSYS
+        else:
+            image_num = int(fn_match.group(1))
+            image_list.del_image(image_num)
+            return -errno.ENOSYS
+
+
+
+    usage = Fuse.fusage
+
+
 
 def main():
-    usage="""
-processfs usage
-
-""" + Fuse.fusage
-    server = HelloFS(version="%prog " + fuse.__version__,
-                     usage=usage,
-                     dash_s_do='setsingle')
-
+    server = ExeFS(version="exefs " + fuse.__version__)
     server.parse(errex=1)
     server.main()
 
