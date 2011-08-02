e3354031790b90d9e02537319de3ece50d8778b7
aont <aont50@gmail.com>
2011-08-02 14:11:28 +0900

    removed MyStat

--- main.py
+++ main.py
@@ -46,23 +46,6 @@ class GPImage:
         gp.stdin.write("exit\n")
         self.data = gp.stdout.read()
     
-class MyStat(fuse.Stat):
-    def __init__(self):
-        self.st_mode = 0
-        self.st_ino = 0
-        self.st_dev = 0
-        self.st_nlink = 0
-        self.st_uid = 0
-        self.st_gid = 0
-        self.st_size = 0
-        self.st_atime = 0
-        self.st_mtime = 0
-        self.st_ctime = 0
-
-
-
-
-
 class ExeFS(Fuse):
     import re
     fn_re = re.compile("/image_(\d+)\.png")
@@ -75,7 +58,7 @@ class ExeFS(Fuse):
         print "getattr:\t%s" % path        
         #fn_match = ExeFS.fn_re.match(path)
         fn_match = self.__class__.fn_re.match(path)
-        st = MyStat()
+        st = fuse.Stat()
         if path == '/':
             st.st_mode = stat.S_IFDIR | 0755
             st.st_nlink = 2
